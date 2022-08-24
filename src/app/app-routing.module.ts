95ec41ea1836b4ade88e6c43d77ff1c64c99bd17
OliverEng <Oliver@Engermann.dk>
2022-08-24 12:37:12 +0200

    Added missing login-page to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TrainerPage } from './pages/trainer/trainer/trainer.page';
 import { PokemonCataloguePage } from './pages/pokemon-catalogue/pokemon-catalogue/pokemon-catalogue.page';
+import { LoginPage } from './pages/login/login/login.page';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     redirectTo: '/login',
+  },
+  {
+    path: 'login',
+    component: LoginPage,
   },
   {
     path: 'trainer',
