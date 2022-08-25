fb683677b2a582af94c2927d617cb15df813dec6
OliverEng <Oliver@Engermann.dk>
2022-08-25 02:31:47 +0200

    Added Navbar
    Added a quick sticky navbar for dev convenience..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { LoginPage } from './pages/login/login/login.page';
 import { TrainerPage } from './pages/trainer/trainer/trainer.page';
 import { PokemonCataloguePage } from './pages/pokemon-catalogue/pokemon-catalogue/pokemon-catalogue.page';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPage,
     TrainerPage,
-    PokemonCataloguePage
+    PokemonCataloguePage,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
